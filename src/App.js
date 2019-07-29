3936e0b53b03ee64fa67a64737f7e2850d4f87f0
Steve Degermark <=>
2019-07-28 17:52:27 -0700

    simplifying to a very dumb app to start using typescript

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
 import React, { useState } from 'react';
-import {useForm} from './useForm';
-
 
 function App() {
 
-  // You can use more than one useState, which allows you to not have to do any Object assigning stuff.
-  // Ben Awad says the way he decides whether to use one view state that operates on objects or two that each operate on primitives
-  // is: if you're updating both values in the same function, use objects and a single useState.  Otherwise, use two useStates
+  // I'm simplifying things a lot and I'm going to switch to typescript.
 
-  const [values, handleChange] = useForm({email: '', password: ''});
+  const [email, setEmail] = useState('a@b.com');
 
   return (
     <div className="App">
       <input
         name='email'
         type="text"
-        value={values.email}
-        onChange={handleChange}
+        value={email}
+        onChange={(e)=> setEmail(e.target.value)}
       />
-      <input
-        name='password'
-        type="password" 
-        value={values.password}
-        onChange={handleChange}
-      />
+      
       <div>
-        <span>{values.email}, {values.password} </span>
+        <span>{email} </span>
       </div>
     </div>
   );
